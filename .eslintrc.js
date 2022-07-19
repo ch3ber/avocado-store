cae5d0ca49f180862fd185af272fc806e5caa52a
Eber Alejo <eber4202@hotmail.com>
2022-07-18 20:48:58 -0500

    [FIX] typescript linter and types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "standard", "next/core-web-vitals"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals'
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
-};
+  plugins: ['react', '@typescript-eslint'],
+  rules: {}
+}
